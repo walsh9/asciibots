29fdfcc0c6b5398f3a753fe25099c91a24595d4f
Matthew Walsh <walsh9@gmail.com>
2014-07-19 19:06:17 -0400

    Add more tests

--- dist/jquery/asciibots.js
+++ dist/jquery/asciibots.js
@@ -73,12 +73,12 @@
       if (radix < 2 || radix > 36) {
         throw "RangeError: radix must be an integer at least 2 and no greater than 36";
       } else if (radix <= 10) {
-        range = "0-" + radix - 1;
+        range = "0-" + String(radix - 1);
       } else {
         range = "0-9a-" + (radix - 1).toString(radix);
       }
       var re = new RegExp("^[" + range + "]{" + minlength + "," + maxlength + "}$");
-      return re.test(id);
+      return re.test(id.toLowerCase());
     },
 
     random: function(length, radix) {
--- test/test-idhelper.js
+++ test/test-idhelper.js
@@ -7,6 +7,9 @@ QUnit.test( "Valid ids are considered valid", function( assert ) {
   assert.ok( idHelper.isValid("fff", 3, 3, 16) );
   assert.ok( idHelper.isValid("FFF", 3, 3, 16) );
   assert.ok( idHelper.isValid("z", 1, 3, 36) );
+  assert.ok( idHelper.isValid("0", 1, 100, 2) );
+  assert.ok( idHelper.isValid("012012", 6, 6, 3) );
+  assert.ok( idHelper.isValid("01234567", 6, 8, 8) );
 });
 
 QUnit.test( "Invalid ids are considered invalid", function( assert ) {
@@ -18,4 +21,52 @@ QUnit.test( "Invalid ids are considered invalid", function( assert ) {
   assert.ok( !idHelper.isValid("fff", 3, 3, 15) );
   assert.ok( !idHelper.isValid("zzz", 3, 3, 35) );
   assert.ok( !idHelper.isValid("", 1, 3, 35) );
+  assert.ok( !idHelper.isValid("10201", 1, 4, 10) );
+  assert.ok( !idHelper.isValid("", 1, 3, 35) );
+});
+
+QUnit.test( "Invalid radix throws exception (isValid)", function( assert ) {
+  assert.throws( function() { idHelper.isValid("999", 3, 5, 1) } );
+  assert.throws( function() { idHelper.isValid("999", 3, 5, 37) } );
+});
+
+QUnit.test( "Invalid radix throws exception (random)", function( assert ) {
+  assert.throws( function() { idHelper.random(20, 1) } );
+  assert.throws( function() { idHelper.random(20, 37) } );
+});
+
+QUnit.test( "Random generates valid Ids", function( assert ) {
+  function countValidIds(iterations, idlength, idradix, pattern) {
+    var count = 0;
+    for (var i = 0; i < iterations; i++) {
+      var id = idHelper.random(idlength, idradix) 
+      if ( pattern.test(id) ) {
+          count++;
+      }
+    }
+    return count;
+  }
+  var iterations = 1000;
+  assert.equal( countValidIds(iterations, 10, 10, /^[0-9]{10}$/), iterations );
+  assert.equal( countValidIds(iterations, 10, 16, /^[0-9a-f]{10}$/), iterations );
+  assert.equal( countValidIds(iterations, 16, 2, /^[0-1]{16}$/), iterations );
+  assert.equal( countValidIds(iterations, 50, 36, /^[0-9a-z]{50}$/), iterations );
+});
+
+QUnit.test( "Random generates all ids within a range", function( assert ) {
+  // Create object of all possible ids.
+  var allNumbers = [];
+  for ( var i = 0x0; i <= 0xfff; i++ ) {
+    var n = (i + 0x1000).toString(16).slice(1, 6);
+    allNumbers.push(n);
+  };
+  // Play "darts" until all ids are gone.
+  var tries = 0;
+  while (allNumbers.length > 0 && tries < 100000 ) {
+    tries++;
+    var random = idHelper.random(3,16); 
+    allNumbers = allNumbers.filter( function(n) { return (n != random); } );
+  };
+  console.log(tries);
+  assert.equal( allNumbers.length, 0 );
 });
